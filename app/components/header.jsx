fdebc33b72ed4ff37a47c763190a6d1e982bc162
erk.cgn <erikac.ec80@gmail.com>
2023-08-25 16:57:40 -0300

    trabajando sobre header, estilos y useLocation

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,24 +1,32 @@
-import { Link } from "@remix-run/react"
+import { Link, useLocation } from "@remix-run/react"
+import logo from '../../public/img/logo.svg'
 
 function Header() {
+    const location = useLocation()
+    
+
   return (
     <header className="header">
         <div className="contenedor barra">
-            <div className="logo">
-
-            </div>
+            <Link to="/">
+                <img className="logo" src={logo} alt="Imagen logo" />
+            </Link>
             <nav className="navegacion">
                 <Link
                     to="/"
+                    className={location.pathname === '/' ? 'active' : ''}
                 >Inicio</Link>
                 <Link
                     to="/nosotros"
+                    className={location.pathname === '/nosotros' ? 'active' : ''}
                 >Nosotros</Link>
                 <Link
                     to="/tienda"
+                    className={location.pathname === '/tienda' ? 'active' : ''}
                 >Tienda</Link>
                 <Link
                     to="/blog"
+                    className={location.pathname === '/blog' ? 'active' : ''}
                 >Blog</Link>
             </nav>
         </div>
