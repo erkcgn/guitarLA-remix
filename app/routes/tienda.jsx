cc5f6ffa2d16454178eb4f67ff7f67846516ada0
erk.cgn <erikac.ec80@gmail.com>
2023-08-28 12:23:50 -0300

    trabajando fetch a strapi

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,5 +1,12 @@
 
 
+export async function loader(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+  const resultado = await respuesta.json()
+
+  return {}
+}
+
 function Tienda() {
   return (
     <div>
