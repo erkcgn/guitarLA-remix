bf90d5d7810a59486a53dd353704839703087dda
erk.cgn <erikac.ec80@gmail.com>
2023-08-28 13:20:03 -0300

    retornando guitarras desde strapi(back), directamente el objeto

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,17 +1,31 @@
-
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarras } from '~/models/guitarras.server'
+import Guitarra from '~/components/guitarra'
 
 export async function loader(){
-  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
-  const resultado = await respuesta.json()
-
-  return {}
+  const guitarras = await getGuitarras()
+  return guitarras.data
 }
 
 function Tienda() {
+  
+  const guitarras = useLoaderData()
+
+
   return (
-    <div>
-      Tienda
-    </div>
+    <main className='contenedor'>
+      <h2 className='heading'>Nuestra Coleccion</h2>
+      {guitarras?.length && (
+        <div className='guitarras-grid'>
+          {guitarras.map(guitarra => (
+            <Guitarra
+              key={guitarra?.id}
+              guitarra={guitarra?.attributes}
+            />
+          ))}
+        </div>
+      )}
+    </main>
   )
 }
 
