6892b76d72f77026482ad17f795685fa633c4c45
erk.cgn <erikac.ec80@gmail.com>
2023-09-07 10:26:20 -0300

    aplique funcion formaterFecha a publishedAt

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,5 +1,6 @@
 
 import { Link } from "@remix-run/react"
+import { formatearFecha } from "~/utils/helpers"
 
 export default function Post({post}) {
 
@@ -10,7 +11,7 @@ export default function Post({post}) {
         <img className="imagen" src={imagen.data.attributes.formats.small.url} alt={`imagen blog ${titulo}`} />
         <div className="contenido">
             <h3>{titulo}</h3>
-            <p className="fecha">{publishedAt}</p>
+            <p className="fecha">{formatearFecha(publishedAt)}</p>
             <p className="resumen">{contenido}</p>
             <Link className="enlace" to={`/posts/${url}`}>Leer Post</Link>
         </div>
