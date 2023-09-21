f3c29a090752c293cf3f3b75a7f2cb48e5d6c575
erk.cgn <erikac.ec80@gmail.com>
2023-09-21 17:33:15 -0300

    agregue estilos seccion curso

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,10 +2,13 @@
 import { useLoaderData } from '@remix-run/react'
 import { getGuitarras } from '~/models/guitarras.server'
 import { getPosts} from '~/models/posts.server'
+import { getCurso} from '~/models/curso.server'
 import ListadoGuitarras from '~/components/listado-guitarras'
 import ListadoPost from '../components/listado-posts'
+import Curso from '../components/curso'
 import stylesGuitarras from '~/styles/guitarras.css'
 import stylesPosts from '~/styles/blog.css'
+import stylesCurso from '~/styles/curso.css'
 
 
 export function meta(){
@@ -21,19 +24,26 @@ export function links(){
     {
       rel: 'stylesheet',
       href: stylesPosts
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesCurso
     }
   ]
 
 }
 
 export async function loader(){
-  const [guitarras, posts] = await Promise.all([
+  const [guitarras, posts, curso] = await Promise.all([
     getGuitarras(),
-    getPosts()
+    getPosts(),
+    getCurso()
   ])
+
   return{
     guitarras: guitarras.data,
-    posts: posts.data
+    posts: posts.data,
+    curso: curso.data
   }
 }
 
@@ -41,13 +51,16 @@ export async function loader(){
 
 function Index() {
 
-  const { guitarras, posts} = useLoaderData();
+  const { guitarras, posts, curso} = useLoaderData();
   
   return (
     <>
       <main className="contenedor">
         <ListadoGuitarras guitarras={guitarras} />
       </main>
+      <Curso
+        curso={curso.attributes}
+      />    
       <section className='contenedor'>
         <ListadoPost posts={posts} />
       </section>
