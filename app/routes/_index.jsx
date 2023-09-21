57a98694382d57debddd6e66d379d62c176295db
erk.cgn <erikac.ec80@gmail.com>
2023-09-21 16:58:28 -0300

    se trabaja index agregando guitarras y posts

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,11 +1,58 @@
 
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarras } from '~/models/guitarras.server'
+import { getPosts} from '~/models/posts.server'
+import ListadoGuitarras from '~/components/listado-guitarras'
+import ListadoPost from '../components/listado-posts'
+import stylesGuitarras from '~/styles/guitarras.css'
+import stylesPosts from '~/styles/blog.css'
+
+
+export function meta(){
+
+}
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: stylesGuitarras
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesPosts
+    }
+  ]
+
+}
+
+export async function loader(){
+  const [guitarras, posts] = await Promise.all([
+    getGuitarras(),
+    getPosts()
+  ])
+  return{
+    guitarras: guitarras.data,
+    posts: posts.data
+  }
+}
+
+
 
 function Index() {
+
+  const { guitarras, posts} = useLoaderData();
+  
   return (
-    <div>
-      Index
-    </div>
-  )
+    <>
+      <main className="contenedor">
+        <ListadoGuitarras guitarras={guitarras} />
+      </main>
+      <section className='contenedor'>
+        <ListadoPost posts={posts} />
+      </section>
+    </>
+  );
 }
 
 export default Index
