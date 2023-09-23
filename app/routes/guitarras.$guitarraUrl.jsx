060fda01969c01703feeb3b02f16b22c372fec39
erk.cgn <erikac.ec80@gmail.com>
2023-09-23 10:30:59 -0300

    nested routes de tienda

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -5,7 +5,6 @@ import {
     Link
 } from '@remix-run/react'
 import { getGuitarra } from '~/models/guitarras.server'
-import styles from '~/styles/guitarras.css'
 
 export async function loader({params}) {
     
@@ -53,28 +52,19 @@ export function meta({data}){
     ]  
 }
 
-export function links(){
-    return [
-        {
-            rel: 'stylesheet',
-            href: styles
-        }
-    ]
-}
-
 function Guitarra() {
 
     const guitarra = useLoaderData()
     const {nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
   return (
-    <main className='contenedor guitarra'>
+    <div className='guitarra'>
         <img className='imagen' src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
         <div className='contenido'>
             <h3>{nombre}</h3>
             <p className='texto'>{descripcion}</p>
             <p className='precio'>${precio}</p>
         </div>
-    </main>
+    </div>
   )
 }
 
--- app/routes/guitarras._index.jsx
+++ app/routes/guitarras._index.jsx
@@ -1,7 +1,6 @@
 import { useLoaderData } from '@remix-run/react'
 import { getGuitarras } from '~/models/guitarras.server'
 import ListadoGuitarras from '../components/listado-guitarras'
-import styles from '~/styles/guitarras.css'
 
 export function meta(){
   return [
@@ -10,32 +9,17 @@ export function meta(){
   ]
 }
 
-export function links(){
-  return [
-    {
-      rel: 'stylesheet',
-      href: styles
-    }
-  ]
-
-}
-
 export async function loader(){
   const guitarras = await getGuitarras()
   return guitarras.data
 }
 
-function Tienda() {
-  
+function Tienda() {  
   const guitarras = useLoaderData()
-
-
   return (
-    <main className='contenedor'>
-      <ListadoGuitarras
-        guitarras={guitarras}
-      />
-    </main>
+    <ListadoGuitarras
+     guitarras={guitarras} 
+    />
   )
 }
 
