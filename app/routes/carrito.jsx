f37ada6249456b2935547c854619b98bfae469e4
erk.cgn <erikac.ec80@gmail.com>
2023-09-25 10:26:31 -0300

    dando estructura, personalizando styles y info de meta

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,11 +1,40 @@
 
 
+import styles from '~/styles/carrito.css'
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
+export function meta(){
+    return [
+        {title: 'GuitarLA - Carrito de Compras'},
+        {description: 'Venta de guitarras, música, blog, carrito de compras, tienda'}
+    ]
+}
 function Carrito() {
   return (
-    <div>
-      Carrito
-    </div>
-  )
+    <main className="contenedor">
+      <h1 className="heading">Carrito de Compras</h1>
+
+      <div className="contenido">
+        <div className='carrito'>
+            <h2>Articulos</h2>
+        </div>
+
+        <aside className="resumen">
+          <h3>Resumen del Pedido</h3>
+          <p>Total a pagar: $</p>
+        </aside>
+      </div>
+
+    </main>
+  );
 }
 
 export default Carrito
