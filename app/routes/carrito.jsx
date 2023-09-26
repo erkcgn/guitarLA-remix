3064ad6c60401b5f6707d79e8b0c41eb6921a654
erk.cgn <erikac.ec80@gmail.com>
2023-09-26 13:58:56 -0300

    destructuring de carrito desde contexto, estructura de la vista

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,5 +1,5 @@
 
-
+import { useOutletContext } from '@remix-run/react';
 import styles from '~/styles/carrito.css'
 
 export function links(){
@@ -18,6 +18,7 @@ export function meta(){
     ]
 }
 function Carrito() {
+  const {carrito} = useOutletContext()
   return (
     <main className="contenedor">
       <h1 className="heading">Carrito de Compras</h1>
@@ -25,6 +26,23 @@ function Carrito() {
       <div className="contenido">
         <div className='carrito'>
             <h2>Articulos</h2>
+            {carrito.length === 0 ? 'Carrito Vacio' : (
+              carrito.map(producto => (
+                <div key={producto.id} className='producto'>
+                  <div>
+                    <img src={producto.imagen} alt={`Imagen del producto ${producto.nombre}`} />
+                  </div>
+
+                  <div className='nombre'>
+                    <p>{producto.nombre}</p>
+                    <p>Cantidad: {producto.cantidad}</p>
+
+                    <p className='precio'>$ <span>{producto.precio}</span></p>
+                    <p className='subtotal'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
+                  </div>
+                </div>
+              ))
+            )}
         </div>
 
         <aside className="resumen">
